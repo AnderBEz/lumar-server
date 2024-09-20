b134345adb91098e741cff3851d5fc5cbeb700f7
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-09-19 21:51:42 -0600

    [error]: internal error 500

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-// import express from "express";
-// import morgan from "morgan"
-
-// const port = 3000;
-// const app = express();
-// app.use(express.json()) //parsear la informacion
-// app.use(morgan("dev"))
-
-// app.get("/", (req, res) => {
-//     res.send("Escuchando");
-    
-// })
-
-// app.post("/submit", (req, res) => {
-//     const {nombre, numeroCel, fecha, hora} = req.body;
-
-//     res.send(`${nombre} ${numeroCel} ${fecha} ${hora}`) 
-// })
-
-// app.listen(port, () => {
-//     console.log(`listen on port: ${port}`);
-    
-// })
 
 import express from 'express';
 import emailRoutes from './routes/emailRoutes.js';
