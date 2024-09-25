57b282fc9acfa2091d19720409c7730e4aa81305
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-09-25 16:49:55 -0600

    [add]: add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv"
 import express from 'express';
 import emailRoutes from './routes/emailRoutes.js';
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', emailRoutes);
 
